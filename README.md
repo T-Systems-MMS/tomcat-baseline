007a143647f951125e4c11ec55c5c9ce28c8656c
Sebastian Gumprich <Sebastian.Gumprich@t-systems.com>
2018-03-29 10:11:16 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # tomcat-baseline
+
+This Inspec Profile tries to ensure compliance of an Apache Tomcat installation along the [Deutsche Telekom, Group IT Security, Security Requirements]().
+
+## Standalone Usage
+
+This Compliance Profile requires [InSpec](https://github.com/chef/inspec) for execution:
+
+```
+$ git clone https://github.com/T-Systems-MMS/tomcat-baseline
+$ inspec exec tomcat-baseline
+```
+
+You can also execute the profile directly from Github:
+
+```
+$ inspec exec https://github.com/T-Systems-MMS/tomcat-baseline
+```
+
+## License and Author
+
+* Author::  Sebastian Gumprich <sebastian.gumprich@t-systems.com>
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ controls/tomcat.rb
@@ -0,0 +1,199 @@
+control "tomcat.dedicated_user" do
+  tag 'ID: 3.10-5/2.1'
+  title 'The application server must run under a dedicated (operating-system) account that only has the permissions required for operation.'
+  describe service('tomcat') do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
+  describe user('tomcat') do
+    it { should exist }
+  end
+  describe group('tomcat') do
+    it { should exist }
+  end
+  describe processes('tomcat') do
+    its('users') { should eq ['tomcat'] }
+  end
+end
+
+#control "tomcat.unused_services_protocols" do
+#  tag 'ID: 3.39-1/2.2'
+#  title 'Unused services and protocols must be deactivated.'
+#  # connectoren in server.xml mit offenen ports vergleichen
+#end
+
+control "tomcat.shutdownport" do
+  tag 'ID: 3.39-2/2.2'
+  title 'If the shutdown port is not needed, it must be deactivated.'
+  describe file('/etc/tomcat/server.xml') do
+    its('content') { should match '<Server port="-1"' }
+  end
+end
+
+control "tomcat.autodeploy" do
+  tag 'ID: 3.39-4/2.2'
+  title 'Automatic deployment of applications must be disabled.'
+  describe file('/etc/tomcat/server.xml') do
+    its('content') { should match 'autoDeploy="false"' }
+    its('content') { should_not match 'autoDeploy="true"' }
+  end
+end
+
+control "tomcat.sensitive_info" do
+  tag 'ID: 3.01-8/2.2'
+  title 'Sensitive information must not be contained in files, outputs or messages that are by unauthorized users accessible.'
+  describe file('/etc/tomcat/server.xml') do
+    its('content') { should_not match 'xpoweredBy="true"' }
+    its('content') { should_not match 'allowTrace="true"' }
+    its('content') { should_not match 'server=" "' }
+  end
+end
+
+control "tomcat.sample_apps" do
+  tag 'ID: 3.39-5/2.2'
+  title 'Sample applications and unnecessary standard tools must be deleted.'
+  describe directory("/usr/share/tomcat/webapps/js-examples") do
+    it { should_not exist }
+  end
+  describe directory("/usr/share/tomcat/webapps/servlet-example") do
+    it { should_not exist }
+  end
+  describe directory("/usr/share/tomcat/webapps/tomcat-docs") do
+    it { should_not exist }
+  end
+  describe directory("/usr/share/tomcat/webapps/balancer") do
+    it { should_not exist }
+  end
+  describe directory("/usr/share/tomcat/webapps/ROOT/admin") do
+    it { should_not exist }
+  end
+  describe directory("/usr/share/tomcat/webapps/examples") do
+    it { should_not exist }
+  end
+  describe directory("/usr/share/tomcat/server/webapps/host-manager") do
+    it { should_not exist }
+  end
+end
+
+#control "tomcat.manager_app" do
+#  tag 'ID: 3.39-6/2.2'
+#  title 'If the “manager” application is used, this must be protected against unauthorized use.'
+#end
+
+control "tomcat.ip_whitelisting" do
+  tag 'ID: 3.39-7/2.2'
+  title 'Access to the application server must only be possible from approved IP addresses (IP whitelisting).'
+  describe file('/etc/tomcat/server.xml') do
+    # mit der xml resource testen!
+    its('content') { should match 'allow=".*"' }
+  end
+end
+
+#control "tomcat.separate_service" do
+#  tag 'ID: 3.39-8/2.2'
+#  title 'For each application, a separate service must be configured within the application server.'
+#    # in webapps nach verzeichnissen suchen und nach den namen in server.xml suchen
+#end
+
+control "tomcat.logging" do
+  tag 'ID: 3.10-8/2.1'
+  title 'Access to the application server must be logged.'
+  describe file("/usr/share/tomcat/conf/logging.properties") do
+    its('content') { should match '.handlers = 1catalina.org.apache.juli.FileHandler, java.util.logging.ConsoleHandler' }
+    its('content') { should match '1catalina.org.apache.juli.FileHandler.level = FINE' }
+    its('content') { should match '2localhost.org.apache.juli.FileHandler.level = FINE' }
+    its('content') { should match 'java.util.logging.ConsoleHandler.level = FINE' }
+    its('content') { should match '.level = INFO' }
+  end
+  describe directory("/usr/share/tomcat/logs") do
+    it { should exist }
+    it { should be_directory }
+    its('mode') { should cmp '0750' }
+    it { should be_owned_by 'tomcat' }
+  end
+end
+
+control "tomcat.directories" do
+  title 'Check for the existance and correct permissions of the tomcat directories'
+  describe directory('/usr/share/tomcat') do
+    it { should exist }
+    it { should be_directory }
+    its('owner') { should eq 'tomcat' }
+    its('group') { should eq 'tomcat' }
+    its('mode') { should cmp'0750' }
+    it { should be_writable.by_user('tomcat') }
+    it { should be_readable.by_user('tomcat') }
+    it { should be_executable.by_user('tomcat') }
+  end
+  describe file('/usr/share/tomcat/conf/web.xml') do
+    it { should exist }
+    it { should be_file }
+    its('owner') { should eq 'tomcat' }
+    its('group') { should eq 'tomcat' }
+    its('mode') { should cmp '0640' }
+    it { should be_writable.by_user('tomcat') }
+    it { should be_readable.by_user('tomcat') }
+    it { should_not be_executable.by_user('tomcat') }
+  end
+  describe file('/usr/share/tomcat/conf/context.xml') do
+    it { should exist }
+    it { should be_file }
+    its('owner') { should eq 'tomcat' }
+    its('group') { should eq 'tomcat' }
+    its('mode') { should cmp '0640' }
+    it { should be_writable.by_user('tomcat') }
+    it { should be_readable.by_user('tomcat') }
+    it { should_not be_executable.by_user('tomcat') }
+  end
+  describe file('/usr/share/tomcat/conf/server.xml') do
+    it { should exist }
+    it { should be_file }
+    its('owner') { should eq 'tomcat' }
+    its('group') { should eq 'tomcat' }
+    its('mode') { should cmp '0640' }
+    it { should be_writable.by_user('tomcat') }
+    it { should be_readable.by_user('tomcat') }
+    it { should_not be_executable.by_user('tomcat') }
+  end
+  describe directory('/etc/tomcat') do
+    it { should exist }
+    it { should be_directory }
+    its('owner') { should eq 'tomcat' }
+    its('group') { should eq 'tomcat' }
+    its('mode') { should cmp '0750' }
+    it { should be_writable.by_user('tomcat') }
+    it { should be_readable.by_user('tomcat') }
+    it { should be_executable.by_user('tomcat') }
+  end
+  describe directory('/var/log/tomcat') do
+    it { should exist }
+    it { should be_directory }
+    its('owner') { should eq 'tomcat' }
+    its('group') { should eq 'tomcat' }
+    its('mode') { should cmp '0750' }
+    it { should be_writable.by_user('tomcat') }
+    it { should be_readable.by_user('tomcat') }
+    it { should be_executable.by_user('tomcat') }
+  end
+  describe directory('/var/lib/tomcat') do
+    it { should exist }
+    it { should be_directory }
+    its('owner') { should eq 'tomcat' }
+    its('group') { should eq 'tomcat' }
+    its('mode') { should cmp '0750' }
+    it { should be_writable.by_user('tomcat') }
+    it { should be_readable.by_user('tomcat') }
+    it { should be_executable.by_user('tomcat') }
+  end
+  describe directory('/var/cache/tomcat') do
+    it { should exist }
+    it { should be_directory }
+    its('owner') { should eq 'tomcat' }
+    its('group') { should eq 'tomcat' }
+    its('mode') { should cmp '0750' }
+    it { should be_writable.by_user('tomcat') }
+    it { should be_readable.by_user('tomcat') }
+    it { should be_executable.by_user('tomcat') }
+  end
+end
